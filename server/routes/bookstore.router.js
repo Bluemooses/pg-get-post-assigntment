3b57be72ecac2abb7e443c8ae5776c0c0ed20c4f
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2020-05-28 01:05:02 -0500

    We doing it

---
+++ server/routes/bookstore.router.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../modules/pool.js');
+
+//starting us off with some books.
+let bookstore = [
+    {
+        title: "You're a wizard 'Arry",
+        author: "Hermoine Granger",
+        published: '1998'
+    },
+    {
+        title: 'This is not',
+        author: 'a real',
+        published: '1220'
+
+    },
+    {
+        title: 'My computer',
+        author: 'Broke',
+        published: 'Tired'
+
+    },
+];
+
+router.get('/', (req, res) => {
+    //query DB
+    let queryText = 'SELECT * FROM "bookstore"';
+    pool.query(queryText)
+        .then((result) => {
+            //sendIt
+            console.log("Result.rows", result.rows);
+            res.send(result.rows);
+        })
+        .catch((err) => {
+            //If we don't hit the DB let everyone know by sendIt 500
+            console.log("error making query", err);
+            res.sendStatus(500)
+        })
+})
+
+
+module.exports = router;
